11872d2c433241537396e34f088db4ea21a265bf
knzphumthawan <knzphumthawan@gmail.com>
2020-04-20 22:40:34 +0700

    initail layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <b-container>
+    <div id="app">
+      <Nav/>
     </div>
-    <router-view/>
-  </div>
+  </b-container>
 </template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
+a {
+  font-weight: bold;
+  color: #000000;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &.router-link-exact-active {
+    color: #42b983;
   }
+}
+a:hover{
+  font-weight: bold;
+  color: #8E5842;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#">
+        Little Brown Book
+      </b-navbar-brand>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link to="/">Home</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/about">About</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style lang="scss">
+  .navbar-brand {
+    font-weight: bold !important;
+    color: #8E5842 !important;
+  }
+</style>
