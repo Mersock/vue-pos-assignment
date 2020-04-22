1f8f670dfaf1a32804909c1683d62c8ef7cb2f66
knzphumthawan <knzphumthawan@gmail.com>
2020-04-22 23:57:46 +0700

    add basket to checkout.

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -5,21 +5,52 @@
         <b-img :src="item.cover" rounded="circle" width="70px" hight="70px"></b-img>
       </b-col>
       <b-col>{{item.title}}</b-col>
-      <b-col>{{item.quantity}}</b-col>
-      <b-col>{{item.priceTotal}}</b-col>
+      <b-col>x{{item.quantity}}</b-col>
+      <b-col><strong>{{item.priceTotal}}</strong></b-col>
     </b-row>
+    <b-row v-if="basketBookItem.length > 0" class="check-out-items">
+      <b-col cols="9" class="text-right">Discount</b-col>
+      <b-col cols="3"><strong>{{summaryDiscount()}}</strong></b-col>
+    </b-row>
+    <b-row v-if="basketBookItem.length > 0" class="check-out-items">
+      <b-col cols="9" class="text-right">Net</b-col>
+      <b-col cols="3"><strong>{{summaryNetPrice()}}</strong></b-col>
+    </b-row>
+    <b-row v-if="basketBookItem.length > 0" class="check-out-items">
+      <b-col cols="9" class="text-right"></b-col>
+      <b-col cols="3">
+        <b-button v-b-modal.payment-modal class="btn-green">Payment</b-button>
+      </b-col>
+    </b-row>
+    <Modal />
   </b-container>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Modal from '@/components/ModalPayment.vue'
 
 export default {
   name: 'Basket',
+  components: {
+    Modal
+  },
   computed: {
     ...mapState([
       'basketBookItem'
     ])
+  },
+  methods: {
+    summaryDiscount () {
+      const discountItems = this.basketBookItem.map(item => item.discount)
+      const sumDiscount = (a, b) => a + b
+      return discountItems.reduce(sumDiscount)
+    },
+    summaryNetPrice () {
+      const netTotalItems = this.basketBookItem.map(item => item.netTotal)
+      const sumNetTotal = (a, b) => a + b
+      return netTotalItems.reduce(sumNetTotal)
+    }
   }
 }
 </script>
